aaed674f2fa7fe541423e9d078024518fd97586d
Ingvar Olsen <ingvarolsen57@hotmail.com>
2025-01-10 01:02:39 +0100

    api.py cleanup

--- web/api.py
+++ web/api.py
@@ -319,42 +319,6 @@ def dns_tunneling_handler():
 ####### DATA HANDLING APIs ##########
 #####################################
 
-###### Virus calls #######
-
-# @api.route('/download')
-# def download_exe():
-#     exe_path = '/var/www/rubberduck/virus/generated_script.exe'  
-#     return send_file(exe_path, as_attachment=True)
-
-
-# @api.route('/update', methods=['POST'])
-# def update():
-#     print("Virus update called!")
-#     data = json.loads(request.data)
-#     print(data)
-#     try:
-#         return jsonify({'message': 'success'})  # Return a valid response
-#     except Exception as e:
-#         print(e)
-#         return jsonify({'message': str(e)})  # Return a v
-
-#####
-
-###### Create virus ######
-# @login_required
-# @api.route('/virusmake', methods=['GET', 'POST'])
-# def virusmake():
-#     virus.test()
-#     try:
-#         virus.generateExe("generated_script.py")
-#         return jsonify({'message': 'success'})  # Return a valid response
-#     except Exception as e:
-#         print(e)
-#         return jsonify({'message': str(e)})  # Return a valid response with the error message
-   
-
-
-
 ##### SAVE DATA APIS #####
 
 @api.route('/savevirus', methods=['POST'])
@@ -418,79 +382,6 @@ def save_virus():
         flash('An error occurred while saving the virus.', category='error')
         return redirect(url_for('auth.logout'))  
 
-## OLD json virus save
-# @api.route('/savevirus', methods=['POST'])
-# @login_required
-# def save_virus():
-#     print("Save virus called")
-#     try:
-#         # Parse request data
-#         # print(request.form.get('name'))
-#         data = json.loads(request.data)
-#         print(data)
-#         user_id = data.get('user_id')  # User ID of the creator
-#         print(user_id)
-#         token = data.get('token')  # Access token for validation
-#         print(token)
-#         name = data.get('name')  # Name of the virus
-#         print(name)
-#         heartbeat_rate = data.get('heartbeat_rate')  # Heartbeat rate of the virus
-#         print(heartbeat_rate)
-#         use_case_settings = data.get('use_case_settings')  # Use cases as a comma-separated string
-        
-#         # Token validation 
-#         if token != userToken:  
-#             return jsonify({'message': 'Invalid token or duplicate virus'}), 403
-
-#         # Save the virus to the database
-#         new_virus = Virus(
-#             name=name,
-#             heartbeat_rate=heartbeat_rate,
-#             use_case_settings=','.join(use_case_settings),  # Ensure it's stored as a comma-separated string
-#             user_id=user_id,
-#             is_alive=True
-#         )
-#         db.session.add(new_virus)
-#         db.session.commit()
-
-#         return jsonify({'message': 'Virus created successfully!'}), 201
-
-#     except Exception as e:
-#         # Log the exception and return an error response
-#         print(f"Error saving virus: {e}")
-#         return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
-
-# ## saves a virus old
-# @api.route('/savevirus', methods=['POST'])
-# def saveVirus():
-#     data = json.loads(request.data)
-#     print(data)
-#     user_id = data['user_id']
-#     token = data['token']
-#     virus_type = data['virus_type']
-#     name = data['name']
-#     heartbeat_rate = data['heartbeat_rate']
-#     print(virus_type)
-#     print(user_id)
-#     print(name)
-#     print(token)
-#     if token == userToken :
-#         try:
-#             db_connect()
-#             curs.execute("INSERT INTO Virus (virus_type, name, heartbeat_rate, user_id) VALUES (?, ?, ?, ?)", (virus_type, name, heartbeat_rate, user_id))
-#             conn.commit()
-#             conn.close()
-#             print("Success")
-#             flash('Virus Created!', category='success')
-#             return jsonify({'message': 'success'})
-#         except Exception as e:
-#             print(e)
-#             return jsonify({'message': e})
-#     else:
-#         print("token not valid or duplicate virus")
-#         flash('Duplicate virus  or wrong access token', category='error')
-#         return jsonify({'message': 'token not valid or duplicate virus '})
-
 
 ## saves a hosts 
 @api.route('/savehost', methods=['POST'])
@@ -525,54 +416,7 @@ def save_host():
 #########################
 ##### GET DATA APIS #####
 #########################
-
-# Migrated this to view.py
-## Get all active virus for the user to display in virus view, needs to be remade together with getvirus, kind of bloat
-# @login_required
-# @api.route('/getactivevirus', methods=['GET'])
-# def getActiveVirus():
-#     print("getactivevirus")
-#     try:
-#         if validate_token():
-#             db_connect()
-#             print("Trying to get virus table data")
-
-#             curs.execute("SELECT * FROM Virus WHERE user_id = ? AND is_alive = 1", (str(current_user.id)))
-#             rows = curs.fetchall()
-
-#             conn.close()
-#             print("jsondump")
-#             print(json.dumps(rows))
-#             return json.loads(json.dumps(rows))
-
-    
-#     except Exception as e:
-#         print(e)
-#         return jsonify({'message': e})
-#     else:
-#         return jsonify({'message': 'token not valid'})
-
-# @login_required
-# @api.route('/getvirus', methods=['GET'])
-# def getVirus():
-#     try:
-#         # Validates session token against DB token
-#         if validate_token():
-#             db_connect()
-#             print("Trying to get virus table data")
-
-#             curs.execute("SELECT * FROM Virus WHERE user_id = ?", (str(current_user.id)))
-#             rows = curs.fetchall()
-
-#             conn.close()
-#             print("jsondump")
-#             print(json.dumps(rows))
-#             return json.loads(json.dumps(rows))
-#     except Exception as e:
-#         print(e)
-#         return jsonify({'message': e})
-#     else:
-#         return jsonify({'message': 'token not valid'})        
+   
 
 @api.route('/gethosts', methods=['GET'])
 def get_hosts():
@@ -652,7 +496,6 @@ def archive_virus():
         # return redirect(url_for('auth.logout'))  
 
 
-
 ####### DELETE APIS ##########
 @api.route('/deletevirus', methods=['POST'])
 @login_required
@@ -701,38 +544,6 @@ def delete_virus():
         return jsonify({'message': f'Error: {str(e)}', 'status': 'error'}), 500
         #return redirect(url_for('views.virus'))  
 
-# Old Api to delete virus with binded executed param
-# @api.route('/deletevirus', methods=['POST'])
-# #@login_required
-# def deleteVirus():
-
-#     virus_id = request.form.get('name')  # Virus name
-#     user= = request.form.get('heartbeat_rate')  # Heartbeat rate
-#     use_case_settings = request.form.getlist('use_case_settings')  # Use cases (list of selected checkboxes)
-
-#     print("deleteVirus called")
-#     data = request.get_json()
-#     print("data:")
-#     print(data)
-#     id = data['virus_id']
-#     user_id = data['user_id']
-#     token = data['token']
-#     print(id)
-#     print(token)
-#     if token == userToken:
-#         try:
-#             db_connect()
-#             print("Trying to delete Virus")
-#             curs.execute("DELETE FROM virus WHERE id = ? AND user_id = ?", (virus_id, user_id))
-#             conn.commit()
-#             conn.close()
-#             print("Virus deleted")
-#             flash('Virus deleted!', category='success')
-#             return jsonify({'message': 'success'})
-#         except Exception as e:
-#             return jsonify({'message': e})
-#     else:
-#         return jsonify({'message': 'token not valid'})
 
 
 @api.route('/deletehost', methods=['DELETE'])
@@ -897,137 +708,3 @@ def internal_virus_download():
 
 
 
-
-
-
-########## Testing area ############
-## Json examples ##
-## Gets used to test APIs with json as data in the calls
-
-exampleUser = {
-    "user_id": 1,
-    "token": "1234567890",
-    "email ": "tester@tester.com",
-    "password ": "Test12345",
-    "public_key ": "shapublic",
-    "private_key ": "shaprivate",
-    }
-
-exampleVirus = {
-    "user_id": 1,
-    "token": "1234567890",
-    "virus_type": "Silent",
-    "name": "TestVirus1",
-    "heartbeat_rate": "1h",
-    "user_id": 1
-    }
-
-exampleHost= {
-    "user_id": 1,
-    "token": "1234567890",
-    "pc_name": "Silent",
-    "country": "TestVirus1",
-    "host_notes": "1h",
-    "settings": "SomeSetting",
-    "last_heartbeat": "10/05/2023;21:45",
-    "host_notes": "1h",
-    "user_id": 1,
-    "virus_id": 1,
-    }
-
-exampleDeleteHost= {
-    "user_id": 1,
-    "token": "1234567890",
-    "id":1,
-    }
-
-exampleDeleteVirus= {
-    "user_id": 1,
-    "token": "1234567890",
-    "id":1,
-    }
-
-## Example Python api call which POST to a remote server api with a json object which uses the exampleJson format in a try catch block
-### Get examples ###
-@api.route('/apivirussendexample', methods=['GET', 'POST'])
-def apiVirusExample():
-    try:
-        url = "http://127.0.0.1:5000/savevirus"
-        headers = {'Content-Type': 'application/json'}
-        response = requests.post(url, data=json.dumps(exampleVirus), headers=headers)
-        return "Success"
-    except Exception as e:
-        print(e)
-        return "Error"
-
-@api.route('/apiupdate', methods=['GET', 'POST'])
-def apiApiExample():
-    try:
-        url = "http://127.0.0.1:5000/update"
-        headers = {'Content-Type': 'application/json'}
-        response = requests.post(url, data=json.dumps(exampleVirus), headers=headers)
-        return "Success"
-    except Exception as e:
-        print(e)
-        return "Error"
-
-@api.route('/apihostssendexample', methods=['GET', 'POST'])
-def apiHostsExample():
-    try:
-        url = "http://127.0.0.1.93:5000/savehost"
-        headers = {'Content-Type': 'application/json'}
-        response = requests.post(url, data=json.dumps(exampleHost), headers=headers)
-        return "Success"
-    except Exception as e:
-        print(e)
-        return "Error"
-### Get examples ###
-@api.route('/apigetvirusexample', methods=['GET', 'POST'])
-def apiGetVirusExample():
-    try:
-        url = "http://127.0.0.1:5000/getvirus"
-        headers = {'Content-Type': 'application/json'}
-        #response = requests.post(url, data=json.dumps(current_user.id), headers=headers)
-        response = requests.get(url, data=json.dumps(current_user.id), headers=headers)
-        return "Success"
-    except Exception as e:
-        print(e)
-        return "Error"
-    
-@api.route('/apigethostsexample', methods=['GET', 'POST'])
-def apiGetHostsExample():
-    try:
-        url = "http://127.0.0.1:5000/gethosts"
-        headers = {'Content-Type': 'application/json'}
-        response = requests.get(url, data=json.dumps(current_user.id), headers=headers)
-        return "Success"
-    except Exception as e:
-        print(e)
-        return "Error"   
-
-### Delete examples ###
-@api.route('/apideletevirusexample', methods=['GET', 'POST'])
-def apiDeleteVirusExample():
-    print("apiDeleteVirusExample")
-    print(current_user.id)
-    try:
-        url = "http://127.0.0.1:5000/deletevirus"
-        headers = {'Content-Type': 'application/json'}
-        #response = requests.post(url, data=json.dumps(current_user.id), headers=headers)
-        response = requests.delete(url, data=json.dumps(exampleDeleteHost), headers=headers)
-        return "Success"
-    except Exception as e:
-        print(e)
-        return "Error"
-    
-@api.route('/apideletehostsexample', methods=['GET', 'POST'])
-def apiDeleteHostsExample():
-    print("apiDeleteHostsExample")
-    try:
-        url = "http://127.0.0.1:5000/deletehost"
-        headers = {'Content-Type': 'application/json'}
-        response = requests.delete(url, data=json.dumps(exampleDeleteHost), headers=headers)
-        return "Success"
-    except Exception as e:
-        print(e)
-        return "Error"   
